a8d85ed1eed84ee26ae1d7aa6de691844d5d372a
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-11 04:56:21 +0900

    Update roi_align_test.py

--- roi_align_test.py
+++ roi_align_test.py
@@ -17,8 +17,18 @@ t_g_data = T.grad(t_c, t_data)[0]
 f = theano.function([t_data, t_rois], t_outs + [t_g_data])
 
 # Perform actual test.
-data = np.random.rand(1, 2, 32, 32).astype(np.single)
-rois = np.array([[0, 0, 0, 3, 3],
-                 [0, 0, 0, 7, 7]]).astype(np.single)
+data = np.array([
+    [0.88, 0.44, 0.14, 0.16, 0.37, 0.77, 0.96, 0.27],
+    [0.19, 0.45, 0.57, 0.16, 0.63, 0.29, 0.71, 0.70],
+    [0.66, 0.26, 0.82, 0.64, 0.54, 0.73, 0.59, 0.26],
+    [0.85, 0.34, 0.76, 0.84, 0.29, 0.75, 0.62, 0.25],
+    [0.32, 0.74, 0.21, 0.39, 0.34, 0.03, 0.33, 0.48],
+    [0.20, 0.14, 0.16, 0.13, 0.73, 0.65, 0.96, 0.32],
+    [0.19, 0.69, 0.09, 0.86, 0.88, 0.07, 0.01, 0.48],
+    [0.83, 0.24, 0.97, 0.04, 0.24, 0.35, 0.50, 0.91],
+], dtype=np.single)
+data = data[np.newaxis, np.newaxis, :, :]
+rois = np.array([[0, 0, 2, 6, 7]]).astype(np.single)
 
 outs = f(data, rois)
+print(outs)
